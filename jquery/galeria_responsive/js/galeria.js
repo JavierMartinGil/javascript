d20ce88cc7487ac1d73e729bfaaca9727bca15ed
javier_martin83 <javier_martin83@yahoo.es>
2019-07-22 14:40:31 +0200

    Final galeria

--- jquery/galeria_responsive/js/galeria.js
+++ jquery/galeria_responsive/js/galeria.js
@@ -4,19 +4,38 @@ trabajo.on('click', cargarModal);
 
 function cargarModal(){
 
-$('body').append(`<div class="modal">
-                        <div class="manto">
-                            <div class="contenedor">
-                                <img src="images/g-architecture.jpg">
-                                <h4>User experience - dinamicos</h4>
-                                <h3>Architechture</h3>
-                                <div class="cerrar">
-                                    <img src="images/cerrar.png" alt="cerrar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>`);
-$('.modal').fadeIn(400);
-$('.modal .contenedor').animate({'top': '4%',})
+    var imagen = $(this).find('img').attr('src');
+    var subtitulo = $(this).find('h4').text();
+    var titulo = $(this).find('h3').text();
 
+    $('body').append(`<div class="modal">
+                            <div class="manto"></div>
+                                <div class="contenedor">
+                                    <img src="${imagen}">
+                                    <h4>${subtitulo}</h4>
+                                    <h3>${titulo}</h3>
+                                    <div class="cerrar">
+                                        <img src="images/cerrar.png" alt="cerrar">
+                                    </div>
+                                </div>
+                            
+                        </div>`);
+    $('.modal').fadeIn(400);
+    $('.modal .contenedor').animate({'top': '4%',})
+    $('.cerrar').on('click', cerrarModal);
+    $(document).on('keydown', cerrarModal);
+    $('body').css('overflow', 'hidden');
+    $('.manto').on('click', cerrarModal);
+
+}
+
+function cerrarModal(e){
+
+    if(e.keyCode == 27 || e.type == "click"){
+        $('.modal .contenedor').animate({'top': '1%'});
+        $('.modal').fadeOut(400, function(){
+            $('.modal').remove();
+        });
+        $('body').css('overflow', 'auto');
+    }
 }
